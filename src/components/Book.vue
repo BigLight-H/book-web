aebe76fb143190166d54b7dab741d65fec3d2654
黄光日 <463817374@qq.com>
2020-04-03 11:08:54 +0800

    设置主题

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -52,7 +52,7 @@
         </Col>
         <Col :xs="{ span: 2}" :lg="{ span: 4}">&nbsp;</Col>
       </Row>
-    <Row class="book-floor">
+    <Row class="book-floor" id="book-floor">
       <Col :xs="{ span: 5, offset: 1 }" :lg="{ span: 5, offset: 6  }" class="book-page">
         <Button type="info">上一章</Button>
       </Col>
@@ -79,26 +79,40 @@
     <Drawer title="设置" :closable="false" v-model="value2">
       <b class="book-latel">背景主题</b>
       <Row>
-        <Col class="book-color" span="6" style="background-color: #feecdc;" id="book-color-1" @onclick="changeBookColor(1)">
-          <div class="book-color-div"><Icon type="ios-checkmark" size="33" style="margin-top: 2px;"/></div>
-        </Col>
-        <Col class="book-color" span="6" style="background-color: #fdf6b2;"></Col>
-        <Col class="book-color" span="6" style="background-color: #f3faf7;"></Col>
-        <Col class="book-color" span="6" style="background-color: #edfafa;"></Col>
-        <Col class="book-color" span="6" style="background-color: #ebf5ff;"></Col>
-        <Col class="book-color" span="6" style="background-color: #f0f5ff;"></Col>
-        <Col class="book-color" span="6" style="background-color: #f6f5ff;"></Col>
-        <Col class="book-color" span="6" style="background-color: #838192;"></Col>
+        <div @click="changeBookColor(1, 'color-1', '#dec9b7')">
+          <Col class="book-color" span="6" style="background-color: #feecdc;" id="book-color-1">
+            <div class="book-color-div"><img :src="imgUrl_require" alt=""></div>
+          </Col>
+        </div>
+        <div @click="changeBookColor(2, 'color-2', '#e2db92')">
+          <Col class="book-color" span="6" style="background-color: #fdf6b2;" id="book-color-2"></Col>
+        </div>
+        <div @click="changeBookColor(3, 'color-3', '#c5f1de')">
+          <Col class="book-color" span="6" style="background-color: #f3faf7;" id="book-color-3"></Col>
+        </div>
+        <div @click="changeBookColor(4, 'color-4', '#b1e0e0')">
+          <Col class="book-color" span="6" style="background-color: #edfafa;" id="book-color-4"></Col>
+        </div>
+        <div @click="changeBookColor(5, 'color-5', '#b4d1ef')">
+          <Col class="book-color" span="6" style="background-color: #ebf5ff;" id="book-color-5"></Col>
+        </div>
+        <div @click="changeBookColor(6,'color-6', '#bdccea')">
+          <Col class="book-color" span="6" style="background-color: #f0f5ff;" id="book-color-6"></Col>
+        </div>
+        <div @click="changeBookColor(7, 'color-7', '#c9c7e2')">
+          <Col class="book-color" span="6" style="background-color: #f6f5ff;" id="book-color-7"></Col>
+        </div>
+        <div @click="changeBookColor(8, 'color-8', '#9d9ab1')">
+          <Col class="book-color" span="6" style="background-color: #838192;" id="book-color-8"></Col>
+        </div>
       </Row>
       <b class="book-latel1">字体样式</b>
       <Row>
-        <Col span="24" style="padding-left: 1rem;">
-          <RadioGroup v-model="button1" type="button">
-            <Radio label="北京"></Radio>
-            <Radio label="上海"></Radio>
-            <Radio label="深圳"></Radio>
-          </RadioGroup>
-        </Col>
+        <Col span="3">&nbsp;</Col>
+        <Col span="6" class="book-font-style">111</Col>
+        <Col span="6" class="book-font-style1">111</Col>
+        <Col span="6" class="book-font-style2">111</Col>
+        <Col span="3">&nbsp;</Col>
       </Row>
       <b class="book-latel1">字体大小</b>
       <Row>
@@ -129,7 +143,7 @@
       return {
         value1: false,
         value2: false,
-        button1: '北京',
+        imgUrl_require: require('.././assets/img/ok.png')
       }
     },
     mounted: function () {
@@ -154,12 +168,15 @@
           document.getElementById('book-content-nav').style.display = 'block';
           document.getElementById('book-content').style.top = '3rem';
         }
-        console.log(scrollTop)
       },
-      changeBookColor(n) {
-        for (let i = 1; i <9 ; i++) {
-          document.getElementById('book-color-'+i).innerText='';
+      changeBookColor(n, color, str) {//修改主题
+        for (let i = 1; i < 9 ; i++) {
+          document.getElementById('book-color-'+i).innerHTML='';
         }
+        let html = '<div class="book-color-div"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACXUlEQVRYR2NkGGDAOMD2M4w6YDQERkNgeIZA6yytsN9//qxkYWF+XpN2XQpfWUP1EGido9nBzccWLacsLHP9wvM31cnXRenmgJa5mpsFBDlMpeQFxR/cfPPu65efq2vTbmbQxQHNszVuiEnySohK8vK/fPrx59tX30/XpFyzJVTUE4yChukaCqwsDJt///6n05B1C0N9wxxtIeZ/fy9LKQiKCwpzMn98953hyb33zxl/M1rW5Fx/SLEDmmaovZCUFxT//fPP/3dvvp6tSblhCjO0YaqGFQs74wo5JUFZHn4Ohn9//zFcOfP0P+N/Juf67Jv7CVkOkscbAg2TVRxYudnWa+pLCIAUP7n3/ufH999u1abd1GuYop7EzsVcJ6ciLM/JzQq268aF559///hXWJ99cy4xlhN0QMt0NWkBcZ4jUvICCjADnz78+Ondq89POLlYeeRVReRY2ZjBUvdvvvn8/cuvhTVpN3KJtZygA0AK2udpr+MT5PCXlONnghn88uknBnEpPnj4fXz7neHpo4+HalKu2ZNiOVEOaGhgYGKV1boqoyigwcvPgWH+z++/Ge5df/3k979/hg3pt95Q3QEgAxsmq2kwszOd1jaW5EFPNpdPPfn7n4HBqiHz1ilSLScqBOApfpq6HzMr40ptIyl4MFw/9/z7z1+/U5uy7ywlx3KSHABS3DpPq4ebhy1LTlmI8/WLz5/fvvg8vyr5Rj65lpPsAEii1DrAws6s9evH3+vVyaQnOnTHEiwJsfmucZp6aH3WzdWU+BymlywHUMPiUQeMhsBoCAyaEAAAAcXOIetGxVwAAAAASUVORK5CYII=" alt=""></div>';
+        document.getElementById('book-color-'+n).innerHTML = html;
+        document.getElementById('book-floor').style.background = str;
+        document.getElementsByTagName('body')[0].className = color; //设置为新的
       }
     },
     destroyed: function () {
@@ -178,7 +195,7 @@
     right: 0 !important;
     left: 0 !important;
     height: 3rem !important;
-    background: #feecdc !important;
+    background: #feecdc;
   }
   .book-page {
     padding: 7px 0;
@@ -257,6 +274,32 @@
     bottom: 0;
     background: #ff000000;
     border-radius: 50%;
+    text-align: center;
+    padding-top: 3px;
+  }
+  .color-1 {
+    background: #feecdc;
+  }
+  .color-2 {
+    background: #fdf6b2;
+  }
+  .color-3 {
+    background: #f3faf7;
+  }
+  .color-4 {
+    background: #edfafa;
+  }
+  .color-5 {
+    background: #ebf5ff;
+  }
+  .color-6 {
+    background: #f0f5ff;
+  }
+  .color-7 {
+    background: #f6f5ff;
+  }
+  .color-8 {
+    background: #838192;
   }
 </style>
 
